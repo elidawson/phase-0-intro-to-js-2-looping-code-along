de394a33aa7810a860ef43488b6eb7179bb2f8b6
Eli Dawson <edawson@alumni.berklee.edu>
2023-03-22 10:49:10 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+//Thank you card creator function
+function writeCards(namesArray, eventName) {
+    const cards = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards;
+  }
+
+//Countdown Function
+function countDown(num) {
+    for (let i = num; i >=0; i--) {
+    console.log(i);
+    }
+}
+countDown(10);
